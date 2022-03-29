e1f91ab3b655678ab01898e97f13ecdd7bf4ff24
strongforce1 <fporquillo18@gmail.com>
2022-03-29 19:01:01 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-890e9f738a0538a2c269.js"
+    "url": "webpack-runtime-836995ead1a061e6b392.js"
   },
   {
     "url": "framework-0f8de3526885dd073c5e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "922e70ccacd2269de7a40a3e11f79cf9"
+    "revision": "81473f26c26e4f373a291603e3ce3121"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f277fb9b28e1661bc3bb.js"
